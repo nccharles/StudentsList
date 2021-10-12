b1e294a3679a77faffb7d984d82778b822cf61de
Charles NDAYISABA <nccharles1@gmail.com>
2021-10-12 16:34:38 +0200

    inital codes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+import React, { useState } from "react";
+const initial=[{
+  nom:"Ernest",
+  prenom:"Rudasingwa",
+  code:124347834
+}]
+const App = () => {
+  const [student, setStudent] = useState(initial);
+  const [name, setName] = useState({});
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if(!name.nom || !name.prenom || !name.code){
+    return  alert("Please Enter the student info")
+    }
+    e.target.reset();
+    setStudent((p) => [...p, name]);
+    setName({})
+    console.log(student)
+  };
+  const onChange = (e) => {
+    setName(obj=>({...obj,[e.target.name]:e.target.value}));
+  };
+  const Array = student.map((i) => <tr><td>{i.nom}</td><td>{i.prenom}</td><td>{i.code}</td></tr>);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onSubmit}>
+        <table>
+          <tr className="rowTitle">
+            <td className="title">Examen</td>
+            <td colspan="3"></td>
+          </tr>
+          <tr className="header"><th>
+          <label>nom: </label>
+            <input type="text" name="nom" onChange={onChange} />
+            </th>
+            <th>
+          <label>Prenom: </label>
+            <input type="text" name="prenom" onChange={onChange} />
+            </th><th>
+          <label>Code: </label>
+            <input type="number" name="code" onChange={onChange} />
+            </th>
+            <th>
+            <button>Ajouter</button>
+            </th></tr>
+            
+        </table>
+        <h1>Liste des etudiants: </h1>
+        <table className="students">
+            <tr><td>Nom</td><td>Prenom</td><td>Code</td></tr>
+            {Array}
+        </table>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
